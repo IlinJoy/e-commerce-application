a203f67c0e97c14944adb0bd3c3dc78ae68bd787
Yana Pavlova <yn-pvlv@yandex.ru>
2025-05-03 16:20:32 +0300

    feat: add constants for backend/frontend client

---
+++ src/utils/constants.ts
@@ -0,0 +1,18 @@
+// Backend Auth Client
+// for user registration
+export const CTP_PROJECT_KEY="frontend-first-project"
+export const CTP_CLIENT_SECRET="a0kSSisnm0A7Yhy6_XJ_ctec1SYFjSt6"
+export const CTP_CLIENT_ID="YRjTJa0cc7bImnTfiZoqU8PD"
+export const CTP_AUTH_URL="https://auth.us-central1.gcp.commercetools.com"
+export const CTP_API_URL="https://api.us-central1.gcp.commercetools.com"
+export const CTP_SCOPES="manage_project:frontend-first-project"
+
+
+// Frontend Auth Client
+// for log in
+export const CLIENT_PROJECT_KEY="frontend-first-project"
+export const CLIENT_CLIENT_SECRET="XJL1-v22Ku5q07gGEvnoxD2uhkLxXit7"
+export const CLIENT_CLIENT_ID="1ZOoNwXN7_DSkZbz4ZDoIcuE"
+export const CLIENT_AUTH_URL="https://auth.us-central1.gcp.commercetools.com"
+export const CLIENT_API_URL="https://api.us-central1.gcp.commercetools.com"
+export const CLIENT_SCOPES="manage_my_quote_requests:frontend-first-project manage_my_payments:frontend-first-project manage_my_profile:frontend-first-project manage_my_quotes:frontend-first-project view_published_products:frontend-first-project manage_my_orders:frontend-first-project manage_my_shopping_lists:frontend-first-project"
