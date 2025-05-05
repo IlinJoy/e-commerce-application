26de2ddc0fc4234e7a1275931b9c486879ad55ea
Yana Pavlova <yn-pvlv@yandex.ru>
2025-05-05 20:14:40 +0300

    feat: make registerCustomer take optional props related to a customer address

--- src/api/clientAuth.ts
+++ src/api/clientAuth.ts
@@ -1,13 +1,34 @@
-import type { CustomerDraft, ErrorResponse } from '@commercetools/platform-sdk';
+import type { Address, CustomerDraft, ErrorResponse } from '@commercetools/platform-sdk';
 import { apiRoot, getCustomerApiRoot } from './platformApi.js';
 import { mapApiErrorToMessage } from '@/utils/mapApiErrorToMessage.js';
 
-const registerCustomer = async (customerData: CustomerDraft) => {
+type RegisterCustomerOptions = {
+  customerData: CustomerDraft;
+  address?: Address;
+  isDefaultShipping?: boolean;
+  isDefaultBilling?: boolean;
+};
+
+const registerCustomer = async ({
+  customerData,
+  address,
+  isDefaultShipping = false,
+  isDefaultBilling = false,
+}: RegisterCustomerOptions) => {
   try {
+    const addresses = address ? [address] : [];
+
+    const body: CustomerDraft = {
+      ...customerData,
+      addresses,
+      defaultShippingAddress: isDefaultShipping ? 0 : undefined,
+      defaultBillingAddress: isDefaultBilling ? 0 : undefined,
+    };
+
     const response = await apiRoot
       .customers()
       .post({
-        body: customerData,
+        body,
       })
       .execute();
 
