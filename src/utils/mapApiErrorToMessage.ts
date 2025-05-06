a70fa9893014e0bc2432081212b6da8f3b7010ae
Yana Pavlova <yn-pvlv@yandex.ru>
2025-05-06 13:11:43 +0300

    fix: change error mapping from messages to error codes

--- src/utils/mapApiErrorToMessage.ts
+++ src/utils/mapApiErrorToMessage.ts
@@ -6,20 +6,22 @@ export const mapApiErrorToMessage = (error: ErrorResponse): never => {
     throw new Error('Something went wrong on our side. Please try again later.');
   }
 
-  switch (error.message) {
+  const errorCode = error.errors?.[0]?.code;
+
+  switch (errorCode) {
     // ↓ when registering already registered user ↓
-    case 'There is already an existing customer with the provided email.':
+    case 'DuplicateField':
       throw new Error('User with this email already exists');
     // ↓ when registering user with empty password ↓
-    case `'password' should not be empty.`:
+    case 'RequiredField':
       throw new Error('Empty password');
     // ↓ when logging in with incorrect credentials ↓
-    case 'Customer account with the given credentials not found.':
+    case 'InvalidCredentials':
       throw new Error('Incorrect email or password');
     // ↓ when logging in with invalid token ↓
     case 'invalid_token':
       throw new Error('Token invalid.');
     default:
-      throw new Error(`Unknown Error: ${error.message}`);
+      throw new Error(`Unknown Error: ${error.errors?.[0]?.message || error.message}`);
   }
 };
