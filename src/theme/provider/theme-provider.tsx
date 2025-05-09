08250b7e573f05e32f3514bd65e1512d86f08cbd
Mariya Ilina <m.polibit@gmail.com>
2025-05-09 18:00:46 +0300

    chore: add eslit rules

--- src/theme/provider/theme-provider.tsx
+++ src/theme/provider/theme-provider.tsx
@@ -3,13 +3,11 @@ import { CssBaseline, StyledEngineProvider, ThemeProvider } from '@mui/material'
 
 export function ThemeAppProvider({ theme, children }: { theme: Theme; children: React.ReactNode }) {
   return (
-    <>
-      <StyledEngineProvider injectFirst>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          {children}
-        </ThemeProvider>
-      </StyledEngineProvider>
-    </>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 }
