10a26f27a309d4acffa8597f723bd332342baffa
Yana Pavlova <yn-pvlv@yandex.ru>
2025-05-09 12:20:11 +0300

    chore: add types for env vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_CTP_AUTH_URL: string;
+  readonly VITE_CTP_API_URL: string;
+  readonly VITE_PROJECT_KEY: string;
+  readonly VITE_CTP_CLIENT_ID: string;
+  readonly VITE_CTP_CLIENT_SECRET: string;
+  readonly VITE_CTP_SCOPES: string;
+  readonly VITE_CLIENT_CLIENT_ID: string;
+  readonly VITE_CLIENT_CLIENT_SECRET: string;
+  readonly VITE_CLIENT_SCOPES: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
