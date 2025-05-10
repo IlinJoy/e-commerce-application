469c8e6d1b05e9ae84caf603860130661d598dff
Mariya Ilina <m.polibit@gmail.com>
2025-05-10 19:38:01 +0300

    chore: add redirect file

--- src/utils/lazy-load.ts
+++ src/utils/lazy-load.ts
@@ -5,7 +5,7 @@ import { lazy } from 'react';
 //оставить или заменить на loadable/component
 export const lazyLoad = <T>(path: string) => {
   return lazy(() =>
-    import(`../pages/${path}/${path}`)
+    import(`./pages/${path}/${path}`)
       .then((module: { [key: string]: ComponentType<T> }) => {
         const moduleExport = Object.keys(module)[0];
         return { default: module[moduleExport] };
