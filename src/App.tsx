7531c2da3f8f85e0bccabbf85fa3abc6f7ffecf8
Mariya Ilina <m.polibit@gmail.com>
2025-05-15 19:12:36 +0300

    fix: remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AppRouter } from './router/app-router';
 import { ThemeAppProvider } from './theme/provider/theme-provider';
 import { responseTheme } from './theme/theme';
