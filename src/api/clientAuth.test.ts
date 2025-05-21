2644f8f5b8d580d3322d2a4b3a9050a0104490da
Mariya Ilina <m.polibit@gmail.com>
2025-05-21 16:40:00 +0300

    feat: add tests

---
+++ src/api/clientAuth.test.ts
@@ -0,0 +1,33 @@
+import { fetchLoggedInCustomer } from './clientAuth';
+import { getCustomerToken, fetchFromApi } from '@/api/platformApi';
+
+vi.mock('@/api/platformApi', () => ({
+  getCustomerToken: vi.fn(),
+  fetchFromApi: vi.fn(),
+}));
+vi.mock('@/utils/mapApiErrorToMessage', () => ({
+  mapApiErrorToMessage: vi.fn(),
+}));
+
+describe('fetchLoggedInCustomer', () => {
+  const mockEmail = 'test@gmail.com';
+  const mockPassword = 'Test123!';
+  const mockToken = 'token';
+  const mockCustomer = { id: '123' };
+  const mockedGetCustomerToken = vi.mocked(getCustomerToken);
+  const mockedFetchFromApi = vi.mocked(fetchFromApi);
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should log in customer', async () => {
+    mockedGetCustomerToken.mockResolvedValue(mockToken);
+    mockedFetchFromApi.mockResolvedValue(mockCustomer);
+
+    const result = await fetchLoggedInCustomer(mockEmail, mockPassword);
+    expect(mockedGetCustomerToken).toHaveBeenCalledWith(mockEmail, mockPassword);
+    expect(mockedFetchFromApi).toHaveBeenCalledWith('/me', mockToken);
+    expect(result).toEqual({ customer: mockCustomer, customerToken: mockToken });
+  });
+});
--- src/setupTest.ts
+++ src/setupTest.ts
@@ -1,1 +1,12 @@
 import '@testing-library/jest-dom';
+import { afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+
+afterEach(() => {
+  vi.restoreAllMocks();
+});
+
+beforeEach(() => {
+  vi.clearAllMocks();
+  cleanup();
+});
