6a8204621ecf6d7eafeba2a32e82af26f678fb61
Mariya Ilina <m.polibit@gmail.com>
2025-06-01 12:40:47 +0300

    fix: sort filter

--- src/components/sort-filter/sort-filter.tsx
+++ src/components/sort-filter/sort-filter.tsx
@@ -4,6 +4,7 @@ import MenuItem from '@mui/material/MenuItem';
 import type { SelectChangeEvent } from '@mui/material/Select';
 import Select from '@mui/material/Select';
 import Typography from '@mui/material/Typography';
+import styles from './sort-filter.module.scss';
 
 export function SortFilter() {
   const { filterParams, setFilterParams } = useCatalogFilters();
@@ -13,13 +14,14 @@ export function SortFilter() {
   };
 
   return (
-    <div>
+    <div className={styles.sortFilter}>
       <Typography>Sort by:</Typography>
-      <Select value={filterParams['sort'] || ''} onChange={handleChange}>
+      <Select displayEmpty value={filterParams.sort || ''} className={styles.select} onChange={handleChange}>
         <MenuItem value={`name.${LANG} asc`}>Name from A to Z</MenuItem>
         <MenuItem value={`name.${LANG} desc`}>Name from Z to A</MenuItem>
         <MenuItem value={`price asc`}>Price from low to high</MenuItem>
         <MenuItem value={`price desc`}>Price from high to low</MenuItem>
+        <MenuItem value={''}>Default</MenuItem>
       </Select>
     </div>
   );
