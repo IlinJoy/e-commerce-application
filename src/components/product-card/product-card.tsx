ce2ba9adb900c2ff6cedb99e794d6c6a20717eb7
OlgaIndykova <ola1234567890dd@gmail.com>
2025-06-10 01:27:03 +0300

    feat: add buttons for adding/removing product to/from cart

--- src/components/product-card/product-card.tsx
+++ src/components/product-card/product-card.tsx
@@ -11,6 +11,14 @@ import { HighlightedText } from '../highlighted-text/highlighted-text';
 import { useNavigate } from 'react-router';
 import { ROUTES } from '@/router/routes';
 import { PriceBlock } from '../price-block/price-block';
+import Button from '@mui/material/Button';
+import { getOrCreateCart } from '@/api/getOrCreateCart';
+import { addProductToCart } from '@/api/addProductToCart';
+import { useToken } from '@/context/token-context';
+import { removeProductFromCart } from '@/api/removeProductFromCart';
+import { useEffect, useState } from 'react';
+import { anonCookieHandler } from '@/services/cookies/cookie-handler';
+import { useToast } from '@/context/toast-provider';
 
 type ProductCardProps = {
   product: ProductProjection;
@@ -28,6 +36,85 @@ export function ProductCard({ product }: ProductCardProps) {
   const image = images?.[0];
   const itemDescription = description?.[LANG] || '';
   const { itemPrice, hasDiscount, ...discountInfo } = mapPrices(prices);
+
+  const [inCart, setInCart] = useState(false);
+  const { showToast } = useToast();
+
+  const { token } = useToken();
+  const currentToken = token || anonCookieHandler.get();
+
+  useEffect(() => {
+    const checkProduct = async () => {
+      if (!currentToken) {
+        return;
+      }
+
+      const cart = await getOrCreateCart(currentToken);
+      const isProductInCart = cart.lineItems.some((item) => item.productId === product.id);
+
+      setInCart(isProductInCart);
+    };
+
+    checkProduct();
+  }, [currentToken, product.id, token]);
+
+  const handleAddToCart = async () => {
+    if (!currentToken) {
+      return;
+    }
+
+    try {
+      const cart = await getOrCreateCart(currentToken);
+
+      const updatedCart = await addProductToCart({
+        token: currentToken,
+        cartId: cart.id,
+        cartVersion: cart.version,
+        productId: product.id,
+        variantId: product.masterVariant.id,
+        quantity: 1,
+      });
+
+      setInCart(true);
+      showToast({ message: 'This product added to cart' });
+
+      //это для удобства, потом удалить
+      console.log(`added: ${product.name[LANG]}, items in cart: ${updatedCart.lineItems.length}`);
+    } catch {
+      showToast({ message: 'Failed to add this product to cart!', isError: true });
+    }
+  };
+
+  const handleRemoveFromCart = async () => {
+    if (!currentToken) {
+      return;
+    }
+
+    try {
+      const cart = await getOrCreateCart(currentToken);
+
+      const lineItemToRemove = cart.lineItems.find((item) => item.productId === product.id);
+
+      if (!lineItemToRemove) {
+        return;
+      }
+
+      const updatedCart = await removeProductFromCart({
+        token: currentToken,
+        cartId: cart.id,
+        cartVersion: cart.version,
+        lineItemId: lineItemToRemove.id,
+      });
+
+      setInCart(false);
+      showToast({ message: 'This product removed from cart' });
+
+      //здесь тоже для удобства, позже удалить
+      console.log(`deleted: ${lineItemToRemove.name[LANG]}, items in cart: ${updatedCart.lineItems.length}`);
+    } catch {
+      showToast({ message: 'Failed to remove this product fron cart!', isError: true });
+    }
+  };
 
   return (
     <Card className={styles.cardWrapper} variant="outlined">
@@ -48,6 +135,16 @@ export function ProductCard({ product }: ProductCardProps) {
           />
         </CardContent>
       </CardActionArea>
+
+      {!inCart ? (
+        <Button onClick={handleAddToCart} className={styles.addBtn}>
+          Add to cart
+        </Button>
+      ) : (
+        <Button onClick={handleRemoveFromCart} className={styles.removeBtn}>
+          Remove from cart
+        </Button>
+      )}
     </Card>
   );
 }
