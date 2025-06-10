deceb7047d7f2aa2cb82d322386b7458c15347f2
Mariya Ilina <m.polibit@gmail.com>
2025-06-10 14:29:41 +0300

    refactor: cart aside

--- src/components/cart-aside/cart-aside.tsx
+++ src/components/cart-aside/cart-aside.tsx
@@ -5,47 +5,38 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { useNavigate } from 'react-router';
 import styles from './cart-aside.module.scss';
+import { CartAsideRow } from './cart-aside-row';
 
 export function CartAside() {
   const { data: customer } = useCustomerQuery();
   const shippingDetails = mapShippingDetails(customer);
   const navigate = useNavigate();
 
-  const renderField = ([key, value]: [string, string]) => (
-    <div key={key}>
-      <span>{key}: </span>
-      {value}
-    </div>
-  );
-
   return (
     <aside className={styles.cartAside}>
       <Typography variant="h4">Shipping Details</Typography>
+
       {shippingDetails ? (
         <div>
-          <div>
-            <span>Contact Name:</span> {shippingDetails.name}
-          </div>
-          <div>
-            <span>Contact Email:</span> {shippingDetails.mail}
-          </div>
+          <CartAsideRow title="Contact Name" info={shippingDetails.name} />
+          <CartAsideRow title="Contact Email" info={shippingDetails.name} />
 
-          <div className={styles.addressDetails}>
-            <span>Shipping Address:</span>
-            {shippingDetails.shippingAddress ? (
-              <div>
-                {Object.entries(shippingDetails.shippingAddress)
-                  .filter(([key]) => key !== 'id')
-                  .map(renderField)}
-              </div>
-            ) : (
-              <div>-</div>
-            )}
-          </div>
+          {shippingDetails.shippingAddress && (
+            <div className={styles.addressDetails}>
+              <span>Shipping Address:</span>
+
+              {Object.entries(shippingDetails.shippingAddress)
+                .filter(([key]) => key !== 'id')
+                .map(([key, value]: [string, string]) => (
+                  <CartAsideRow key={key} title={key} info={value} />
+                ))}
+            </div>
+          )}
         </div>
       ) : (
-        <div>No shipping details available</div>
+        <Typography>No shipping details available</Typography>
       )}
+
       <Button onClick={() => navigate(`/${ROUTES.ACCOUNT.path}`)}>Update Info</Button>
     </aside>
   );
